0c076ebef808320c4d54f3a3532772076bfda4b3
Harimurti <hariimurti@gmail.com>
2019-02-11 20:13:35 +0700

    foto profil bundar

--- app/src/main/java/net/harimurti/joylive/ListAdapter.java
+++ app/src/main/java/net/harimurti/joylive/ListAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -19,11 +18,13 @@ import net.harimurti.joylive.Classes.Preferences;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ListAdapter extends ArrayAdapter<JoyUser> {
     private Context context;
 
     private class ViewHolder {
-        ImageView image;
+        CircleImageView image;
         TextView nickname;
         TextView status;
         TextView viewer;
