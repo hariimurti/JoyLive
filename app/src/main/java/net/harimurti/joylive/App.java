cb06344b9036239e444efece192bb4fcb359b6d7
Harimurti <hariimurti@gmail.com>
2019-03-03 14:09:47 +0700

    app: get android version & device name

--- app/src/main/java/net/harimurti/joylive/App.java
+++ app/src/main/java/net/harimurti/joylive/App.java
@@ -2,6 +2,7 @@ package net.harimurti.joylive;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 
 public class App extends Application {
     private static Context context;
@@ -13,5 +14,13 @@ public class App extends Application {
 
     public static Context getContext() {
         return App.context;
+    }
+
+    public static String getAndroidVersion() {
+        return Build.VERSION.RELEASE;
+    }
+
+    public static String getDeviceName() {
+        return String.format("%s %s", Build.BRAND, Build.MODEL).trim();
     }
 }
